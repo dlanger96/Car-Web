1ac69eb231e143b11c9bada4e335607cdf047c77
dlanger96 <67539028+dlanger96@users.noreply.github.com>
2021-05-09 22:50:17 +0200

    Api communication

---
+++ Car-Web/Controllers/Api/CarsController.cs
@@ -0,0 +1,23 @@
+﻿using Car_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace Car_Web.Controllers.Api
+{
+    public class CarsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetAllCars()
+        {
+            List<CarsModel> listCars = new List<CarsModel> { new CarsModel { Id_Car= 1, Make="VW", Model="Golf 8"}, 
+                new CarsModel { Id_Car = 2, Make = "BMW", Model = "320d E90" }, 
+                new CarsModel { Id_Car = 3, Make = "Audi", Model = "A4" } };
+            return Ok(listCars);
+        }
+    }
+}
---
+++ Car-Web/Models/CarsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Car_Web.Models
+{
+    public class CarsModel
+    {
+        public int Id_Car { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
