f7d23552dd388608a7c480bf97de10692445fd6a
dlanger96 <67539028+dlanger96@users.noreply.github.com>
2021-05-15 11:24:31 +0200

    Edit slected car by Id inside modal window.

--- Car-Web/Controllers/Api/CarsController.cs
+++ Car-Web/Controllers/Api/CarsController.cs
@@ -36,12 +36,39 @@ namespace Car_Web.Controllers.Api
                 
                 
             }
-            //List<CarsModel> listCars = new List<CarsModel> { new CarsModel { Id_Car= 1, Make="VW", Model="Golf 8"}, 
-            //    new CarsModel { Id_Car = 2, Make = "BMW", Model = "320d E90" }, 
-            //    new CarsModel { Id_Car = 3, Make = "Audi", Model = "A4" } };
-
             return Ok(listOfCars);
         }
+        [HttpGet]
+        public IHttpActionResult GetSelectedCar(int selectedId)
+        {
+            List<CarsModel> listOfCars = new List<CarsModel>();
+
+            List<Car> listCar = new List<Car>();
+            using (CarsEntities entities = new CarsEntities())
+            {
+                listCar = new List<Car>(entities.Cars.Where(c => selectedId == c.Id_Car));
+                foreach (var item in listCar)
+                {
+                    CarsModel carsModel = new CarsModel();
+                    carsModel.Id_Car = item.Id_Car;
+                    carsModel.Fuel = entities.Fuels.Where(c => item.Fuel == c.Id_Fuel).Select(c => c.Name).FirstOrDefault().ToString();
+                    carsModel.Make = entities.Makes.Where(c => c.Id_Make == item.Make).Select(c => c.Name).FirstOrDefault().ToString();
+                    carsModel.Model = entities.Models.Where(c => c.Id_Model == item.Model).Select(c => c.Name).FirstOrDefault().ToString();
+                    carsModel.Power = item.Power;
+                    carsModel.ProductionYear = int.Parse(item.Production_Year.ToString());
+                    carsModel.Quantity = int.Parse(item.Quantity.ToString());
+                    listOfCars.Add(carsModel);
+                }
+
+
+
+            }
+            return Ok(listOfCars);
+        }
+
+
+
+
 
     }
 }
